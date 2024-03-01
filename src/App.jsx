9aef7dd5ddeeb31e02edf27fdcdfdd7b5f26891d
Favour Daniel <favourdaniel74@gmail.com>
2024-03-01 02:31:05 +0100

    Changed token to Token.jsx in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 import Register from './pages/Register/Register.jsx'
 import Login from './pages/Login/Login.jsx'
 import { Route, Routes } from 'react-router-dom'
-import Token from './pages/Register/token.jsx'
+import Token from './pages/Register/Token.jsx'
 import CountryFlag from './component/CountryFlag.jsx'
 function App() {
   return (
